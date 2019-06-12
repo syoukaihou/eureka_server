0bdb9d5ddabf925bfcde84afaa65e937dffd57d0
syoukaihou@gmail.com <syoukaihou@gmail.com>
2019-06-12 17:49:28 +0800

    更新spring cloud 版本

--- src/main/java/com/snsprj/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/snsprj/eureka_server/EurekaServerApplication.java
@@ -2,6 +2,9 @@ package com.snsprj.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer

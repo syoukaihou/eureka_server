95b21876f34b82f2002da7950dc04ee9bfa2a2df
win10 <syoukaihou@gmail.com>
2019-06-17 23:56:04 +0800

    删除不必要的继承

--- src/main/java/com/snsprj/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/snsprj/eureka_server/EurekaServerApplication.java
@@ -2,20 +2,13 @@ package com.snsprj.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication extends SpringBootServletInitializer {
+public class EurekaServerApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(EurekaServerApplication.class);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
